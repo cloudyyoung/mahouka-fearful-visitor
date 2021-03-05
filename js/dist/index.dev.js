6766f94cfbe0693d71fe4be144020c3cfd22e958
Cloudy Young <cloudyyoung74@outlook.com>
2021-03-05 12:03:25 +0800

    Update index

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -1,5 +1,7 @@
 "use strict";
 
+$("body").append("\n    <div class=\"bg-main animated fadeIn\"></div>\n    <div class=\"logo animated fadeIn delay-1s\"></div>\n    <div class=\"chara animated fadeIn delay-1s\"></div>\n    <div class=\"flowers animated fadeIn\"></div>\n");
+
 for (var t = 0; t < 20; t++) {
   $(".flowers").append("<div class=\"flower ".concat(t, "\"><p></p></div>"));
 }
--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 
+$("body").append(`
+    <div class="bg-main animated fadeIn"></div>
+    <div class="logo animated fadeIn delay-1s"></div>
+    <div class="chara animated fadeIn delay-1s"></div>
+    <div class="flowers animated fadeIn"></div>
+`);
+
 for (let t = 0; t < 20; t++) {
     $(".flowers").append(`<div class="flower ${t}"><p></p></div>`);
 }
