8bcdb2d92d42f1a61e6458393ad5450a341835a5
Cloudy Young <cloudyyoung74@outlook.com>
2021-03-05 10:51:26 +0800

    Update index.html

---
+++ js/dist/index.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+for (var t = 0; t < 20; t++) {
+  $(".flowers").append("<div class=\"flower ".concat(t, "\"><p></p></div>"));
+}
---
+++ js/index.js
@@ -0,0 +1,4 @@
+
+for (let t = 0; t < 20; t++) {
+    $(".flowers").append(`<div class="flower ${t}"><p></p></div>`);
+}
